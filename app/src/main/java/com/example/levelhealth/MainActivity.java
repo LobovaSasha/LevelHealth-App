c0596c72aa61dda96f8678bfc62c177123994c07
xenon136 <xenon136@ya.ru>
2022-10-22 23:13:27 +0300

    Fix themes

--- app/src/main/java/com/example/levelhealth/MainActivity.java
+++ app/src/main/java/com/example/levelhealth/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.levelhealth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.BadParcelableException;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button, button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
+        button11 = findViewById(R.id.button11);
+        button.callOnClick();
     }
 }
