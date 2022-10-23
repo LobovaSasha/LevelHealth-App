04677d1689b8a75ea3f29d770cc1d536dc131137
xenon136 <xenon136@ya.ru>
2022-10-23 23:22:46 +0300

    Add switching from main window to all windows

--- app/src/main/java/com/example/levelhealth/ConfirmationActivity.java
+++ app/src/main/java/com/example/levelhealth/ConfirmationActivity.java
@@ -10,6 +10,6 @@ public class ConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading);
+        setContentView(R.layout.activity_confirmation);
     }
 }
--- app/src/main/java/com/example/levelhealth/MainActivity.java
+++ app/src/main/java/com/example/levelhealth/MainActivity.java
@@ -20,4 +20,48 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CalendarActivity.class);
         startActivity(intent);
     }
+    public void GoToChangeWindowActivity(View view) {
+        Intent intent = new Intent(this, ChangeWindowActivity.class);
+        startActivity(intent);
+    }
+    public void GoToChartsActivity(View view) {
+        Intent intent = new Intent(this, ChartsActivity.class);
+        startActivity(intent);
+    }
+    public void GoToConfirmationActivity(View view) {
+        Intent intent = new Intent(this, ConfirmationActivity.class);
+        startActivity(intent);
+    }
+    public void GoToLoadingActivity(View view) {
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
+    }
+    public void GoToMenuActivity(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+    public void GoToNotificationActivity(View view) {
+        Intent intent = new Intent(this, NotificationActivity.class);
+        startActivity(intent);
+    }
+    public void GoToPasswordChangeActivity(View view) {
+        Intent intent = new Intent(this, PasswordChangeActivity.class);
+        startActivity(intent);
+    }
+    public void GoToPasswordRecoveryActivity(View view) {
+        Intent intent = new Intent(this, PasswordRecoveryActivity.class);
+        startActivity(intent);
+    }
+    public void GoToRegistrationActivity(View view) {
+        Intent intent = new Intent(this, RegistrationActivity.class);
+        startActivity(intent);
+    }
+    public void GoToSignInActivity(View view) {
+        Intent intent = new Intent(this, SignInActivity.class);
+        startActivity(intent);
+    }
+    public void GoToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
