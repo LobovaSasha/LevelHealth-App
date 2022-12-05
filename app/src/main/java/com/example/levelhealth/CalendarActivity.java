a3e6a9a1d432e556acb6301964df86aa3d51b7a3
xenon136 <xenon136@ya.ru>
2022-12-05 10:49:16 +0300

    add calendar function

--- app/src/main/java/com/example/levelhealth/CalendarActivity.java
+++ app/src/main/java/com/example/levelhealth/CalendarActivity.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class CalendarActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -17,6 +22,27 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         mAuth = FirebaseAuth.getInstance();
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        TextView date = findViewById(R.id.textView12);
+        date.setVisibility(View.INVISIBLE);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year,
+                                            int month, int dayOfMonth) {
+                TextView date = findViewById(R.id.textView12);
+                int mYear = year;
+                int mMonth = month;
+                int mDay = dayOfMonth;
+                String selectedDate = new StringBuilder().append(mDay)
+                        .append("-").append(mMonth + 1)
+                        .append("-").append(mYear)
+                        .append(" ").toString();
+                date.setText(selectedDate);
+                date.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
     @Override
