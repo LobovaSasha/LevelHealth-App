5e67fff85af07f18a660ec94ff44ac1cc6b2cae5
xenon136 <xenon136@ya.ru>
2022-12-05 11:13:18 +0300

    add menu and registration functions

--- app/src/main/java/com/example/levelhealth/MenuActivity.java
+++ app/src/main/java/com/example/levelhealth/MenuActivity.java
@@ -1,14 +1,21 @@
 package com.example.levelhealth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -19,6 +26,31 @@ public class MenuActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        FirebaseUser cUser = mAuth.getCurrentUser();
+        String id = cUser.getUid();
+        TextView FioText = findViewById(R.id.NameSurname);
+        TextView BirthText = findViewById(R.id.DateOfBirth);
+        TextView EmailText = findViewById(R.id.Mail);
+        String email = cUser.getEmail();
+        EmailText.setText(email);
+
+        final DatabaseReference ref;
+        ref = FirebaseDatabase.getInstance().getReference("User").child(id);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String fio = snapshot.child("Name").getValue().toString();
+                String fio2 = snapshot.child("Surname").getValue().toString();
+
+                FioText.setText(fio + " " + fio2);
+                String birth = snapshot.child("Birth").getValue().toString();
+                BirthText.setText(birth);
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
     }
 
     protected void onStart() {
