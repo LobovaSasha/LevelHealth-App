3d04794a27c5b65b075090a33650a913bc91efa5
sunyasfck <alexrait2014@mail.ru>
2023-03-13 12:06:50 +0300

    fixed date format on notes page

--- app/src/main/java/com/example/levelhealth/NoteActivity.java
+++ app/src/main/java/com/example/levelhealth/NoteActivity.java
@@ -8,24 +8,29 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class NoteActivity extends AppCompatActivity {
 
     Button backBtn;
-    TextView dateTV, readyBtn, titleTV, noteTV;
 
-    final String DAY_KEY = "day", MONTH_KEY = "month", NEW_KEY = "new";
+    private TextView dateTimeDisplay;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String date;
 
-    String[] months = new String[]{
-            "января", "февраля", "марта", "апреля",
-            "мая", "июня", "июля", "августа",
-            "сентября", "октября", "ноября", "декабря"};
 
     private void init() {
         backBtn = findViewById(R.id.back);
-        dateTV = findViewById(R.id.dateText);
-        readyBtn = findViewById(R.id.ready);
-        titleTV = findViewById(R.id.titleText);
-        noteTV = findViewById(R.id.noteText);
+
+        dateTimeDisplay = findViewById(R.id.dateText);
+
+//        dateTV = findViewById(R.id.dateText);
+//        readyBtn = findViewById(R.id.ready);
+//        titleTV = findViewById(R.id.titleText);
+//        noteTV = findViewById(R.id.noteText);
     }
 
     @Override
@@ -37,31 +42,19 @@ public class NoteActivity extends AppCompatActivity {
         backBtn.setOnClickListener(v -> {
             onBackPressed();
         });
+
+        calendar = Calendar.getInstance();
+
+        dateFormat = new SimpleDateFormat("dd MMMM", new Locale("ru") );
+        date = dateFormat.format(calendar.getTime());
+        dateTimeDisplay.setText(date);
+
     }
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onStart() {
         super.onStart();
-
-        Bundle args = getIntent().getExtras();
-        boolean isNewNote = false;
-        if (args != null) {
-            try {
-                // numericDate == "08.04.2023"
-                String numDay = args.getString(DAY_KEY);
-                String numMonth = args.getString(MONTH_KEY);
-                int idMonth = Integer.parseInt(numMonth) - 1;
-                dateTV.setText(numDay+" "+months[idMonth]);
-            } catch (Exception e) {
-                Log.d("DATE_ERROR", e.toString());
-                dateTV.setText("");
-            }
-
-            try {
-                isNewNote = args.getBoolean(NEW_KEY);
-            } catch (Exception e) {}
-        }
 
     }
 }
