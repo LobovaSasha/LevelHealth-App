be52ebc277921897b7f83a670851ae86fcf5d842
sunyasfck <alexrait2014@mail.ru>
2023-03-13 12:48:02 +0300

    added home button functionality on page settings

--- app/src/main/java/com/example/levelhealth/SettingsActivity.java
+++ app/src/main/java/com/example/levelhealth/SettingsActivity.java
@@ -3,9 +3,12 @@ package com.example.levelhealth;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
@@ -122,6 +125,11 @@ public class SettingsActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void GoToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
