aaee438c0c18b02dac3298d0cae07dbb07c661c6
xenon136 <xenon136@ya.ru>
2023-04-03 12:51:10 +0300

    add classes

--- app/src/main/java/com/example/levelhealth/MainActivity.java
+++ app/src/main/java/com/example/levelhealth/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.levelhealth;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
